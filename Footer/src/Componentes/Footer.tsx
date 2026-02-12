a5c8c6f857d224d4c814b05f702fbb802ae0d350
Nahuel <nahuel.dmz@outlook.com>
2026-02-12 13:41:05 -0300

    Empezando footer reusable

---
+++ Footer/src/Componentes/Footer.tsx
@@ -0,0 +1,28 @@
+export function Footer (){
+    return(
+        <footer>
+            <ul className="footer-list">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul className="footer-list">
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <p></p>
+            <ul className="footer-iconos">
+                <li>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <a href="#"></a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
