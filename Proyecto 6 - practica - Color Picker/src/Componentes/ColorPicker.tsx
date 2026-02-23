06ef0e96e3b1d674762c820c4b035c5f34fc0f50
Nahuel <nahuel.dmz@outlook.com>
2026-02-23 02:04:05 -0300

    Color Picker fin

---
+++ Proyecto 6 - practica - Color Picker/src/Componentes/ColorPicker.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+const ColorPicker = () => {
+  const [color, setColor] = useState("#ffffff");
+
+  return (
+    <div
+      id="color-picker-container"
+      style={{ backgroundColor: color }}
+    >
+      <input
+        id="color-input"
+        type="color"
+        value={color}
+        onChange={(e) => setColor(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default ColorPicker;
