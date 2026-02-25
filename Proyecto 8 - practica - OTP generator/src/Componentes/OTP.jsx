802c3e613fc3dff48a9ff9963bfd3020d0034cff
Nahuel <nahuel.dmz@outlook.com>
2026-02-25 19:22:10 -0300

    Handle Submit fin

--- Proyecto 8 - practica - OTP generator/src/Componentes/OTP.jsx
+++ Proyecto 8 - practica - OTP generator/src/Componentes/OTP.jsx
@@ -8,40 +8,49 @@ const OTPGenerator = () => {
     /*USEREF */
     const intervalRef = useRef(null)
     /*FIN USEREF */
-    
+
 
     /*  FUNCION SUBMIT */
 
     const handleSubmit = () => {
+
+        if (intervalRef.current != null) return
+
+        setTime(5)
+
         intervalRef.current = setInterval(
             () => {
-            setTime(prevTime => {
-                if(prevTime === 1){
-                    clearInterval(intervalRef.current)
-                    return prevTime    
-                }
-                return prevTime - 1
-            })
-        },
+                setTime(prevTime => {
+                    if (prevTime === 1) {
+                        clearInterval(intervalRef.current)
+                        intervalRef.current = null
+                        return 0
+                    }
+                    return prevTime - 1
+                })
+            },
             1000)
     }
 
-    /*USE EFFECT*/
-    useEffect(() => {
 
-    }, [contador])
-    /*FIN USE EFFECT*/
+}
+
+/*USE EFFECT*/
+useEffect(() => {
+
+}, [contador])
+/*FIN USE EFFECT*/
 
 
 
 
-    return (
-        <div>
-            <h1>OTP Generador</h1>
-            <p>CLickea en el boton para generar tu clave unica</p>
-            <button>Generar OTP</button>
-        </div>
-    )
+return (
+    <div>
+        <h1>OTP Generador</h1>
+        <p>CLickea en el boton para generar tu clave unica</p>
+        <button>Generar OTP</button>
+    </div>
+)
 
 };
 
